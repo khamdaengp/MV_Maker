40bb47908a1b7fabdc3ce545ac97e04643a0a30a
khamdaengp <khamdaeng2022@gmail.com>
2026-09-20 05:30:31 +0700

    Add push.bat helper script

---
+++ push.bat
@@ -0,0 +1,9 @@
+@echo off
+title Push MV Maker to GitHub
+echo ===================================================
+echo               Pushing to GitHub
+echo ===================================================
+echo.
+git push -u origin main
+echo.
+pause
